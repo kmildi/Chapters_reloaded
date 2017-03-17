8c45f461d1fae417ad60637c1faad1c188ce73db
kmildi <keleti.ildi@gmail.com>
2017-03-17 17:11:02 +0100

    HTML, CSS, JavaScript különválasztás

---
+++ chapters_js.js
@@ -0,0 +1,92 @@
+﻿ 
+   function iconMenu() {
+  	var x = document.getElementById("cimsor");
+	if (x.className === "navbar") {
+	   x.className += " responsive";
+	} else {
+	   x.className = "navbar";
+	}
+   }
+	  
+  initSmoothScrolling();
+
+  function initSmoothScrolling() {
+     document.querySelectorAll('a')
+     .forEach(function(a) {
+        a.addEventListener('click', onClick, false);
+     });
+     function onClick(e) {
+      	  e.stopPropagation();
+      	  e.preventDefault();
+      	  jump(e.target.hash, {
+        	duration: 500,
+      	  });
+      }
+  }
+
+  function jump(target, options) {
+    var
+    start = window.pageYOffset,
+    opt = {
+      duration: options.duration,
+      offset: options.offset || 0,
+      callback: options.callback,
+      easing: options.easing || easeInOutQuad
+    },
+    distance = typeof target === 'string' ?
+    opt.offset + document.querySelector(target).getBoundingClientRect().top :
+    target,
+    duration = typeof opt.duration === 'function' ?
+    opt.duration(distance) :
+    opt.duration,
+    timeStart, timeElapsed;
+
+    requestAnimationFrame(function(time) {
+      timeStart = time;
+      loop(time);
+    });
+ 
+    function loop(time) {
+      timeElapsed = time - timeStart;
+      window.scrollTo(0, opt.easing(timeElapsed, start, distance, duration));
+      if (timeElapsed < duration)
+        requestAnimationFrame(loop)
+      else
+        end();
+    }
+
+    function end() {
+      window.scrollTo(0, start + distance);
+      if (typeof opt.callback === 'function')
+        opt.callback();
+    }
+
+    // Robert Penner's easeInOutQuad - http://robertpenner.com/easing/
+    function easeInOutQuad(t, b, c, d) {
+        t /= d / 2
+      if (t < 1) return c / 2 * t * t + b
+        t--
+       return -c / 2 * (t * (t - 2) - 1) + b
+    }
+  }	  
+	  
+	  
+  (function() {
+    'use strict';
+    var section = document.querySelectorAll(".chapter");
+    var sections = {};
+    var i = 0;
+    Array.prototype.forEach.call(section, function(e) {
+      sections[e.id] = e.offsetTop-250;
+    });
+    window.onscroll = function() {
+      var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+      for (i in sections) {
+        if (sections[i] <= scrollPosition) {
+          document.querySelector('.active').setAttribute('class', ' ');
+          document.querySelector('a[href*=' + i + ']').setAttribute('class', 'active');
+        }
+      }
+    };
+  })();  
+	  
